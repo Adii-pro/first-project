649182c0047099fbd0d845ee559bfe07965b6730
Adii-pro <adityagond49079@gmail.com>
2025-01-02 00:51:46 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-3xl font-bold text-center text-blue-600">
+        Welcome to First Project!
+      </h1>
+      <p className="text-center mt-4 text-gray-700">
+        Your ultimate student life manager.
+      </p>
+    </div>
+  );
+}
+
+export default App;
